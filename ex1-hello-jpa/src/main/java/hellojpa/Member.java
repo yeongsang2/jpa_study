1517c23bbb66b153aefaa08c2a60bd7e5ecdc4d4
yeongsang2 <dudtkd1221@gmail.com>
2022-06-26 23:02:12 +0900

    first jpa introductino

---
+++ ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -0,0 +1,28 @@
+package hellojpa;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity //jpa를 사용하는 애구나~내가 관리해야지
+public class Member {
+
+    @Id //pk 가 뭔지 알려줌
+    private Long id;
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
