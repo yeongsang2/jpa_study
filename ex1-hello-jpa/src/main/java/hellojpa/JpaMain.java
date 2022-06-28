b24a76abfede0f5faa505fbbc8124ce4d60f4df4
yeongsang2 <dudtkd1221@gmail.com>
2022-06-28 14:01:10 +0900

    [엔티티 매핑] 데이터베이스 스키마 자동생성, 필드와 컬럼 매핑

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -16,18 +16,11 @@ public class JpaMain {
         tx.begin();
 
         try {
-            //비영속 상태
-
-            //Member member1 = new Member(150L, "A");
-            //Member member2 = new Member(160L, "Bs");
-            Member findMember = em.find(Member.class, 150L);
-            findMember.setName("xxxx");
-            //em.detach(findMember); 영속상태의 entity가 영속성 컨텍스트에서 분리 detach <특저 entity만 준영속 상태로 전환 >
-            //em.clear(); //영속성 컨텍스트 완전히 초기화
-
-            //em.persist(member1);
-            //em.persist(member2); //영속성 컨텍스트에 쌓임
-            System.out.println("++++");
+            Member member = new Member();
+            member.setId(2L);
+            member.setUsername("B");
+            member.setRoleType(RoleType.ADMIN);
+            em.persist(member);
             tx.commit(); //transaction.commit() 하는 시점에 database 에 query 날림
         } catch (Exception e){
             tx.rollback();
--- ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -1,23 +1,28 @@
 package hellojpa;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
+import javax.transaction.Transactional;
+import java.util.Date;
 
 @Entity //jpa를 사용하는 애구나~내가 관리해야지
 public class Member {
 
-    @Id //pk 가 뭔지 알려줌
+    @Id
     private Long id;
-    private String name;
+    @Column(name = "name", nullable = false) //column 변경되었을때 반영할껀지 여부
+    private String username;
+    private Integer age;
+    @Enumerated(EnumType.STRING) //EnumType.STRING 필수
+    private RoleType roleType;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdDate;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
+    @Lob
+    private String description;
 
-    public Member(){
-
-    }
-
-    public Member(Long id, String name) {
-        this.id = id;
-        this.name = name;
-    }
+    @Transient
+    private int temp;
 
     public Long getId() {
         return id;
@@ -27,11 +32,52 @@ public class Member {
         this.id = id;
     }
 
-    public String getName() {
-        return name;
+    public String getUsername() {
+        return username;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setUsername(String username) {
+        this.username = username;
     }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public RoleType getRoleType() {
+        return roleType;
+    }
+
+    public void setRoleType(RoleType roleType) {
+        this.roleType = roleType;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public Date getLastModifiedDate() {
+        return lastModifiedDate;
+    }
+
+    public void setLastModifiedDate(Date lastModifiedDate) {
+        this.lastModifiedDate = lastModifiedDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
 }
