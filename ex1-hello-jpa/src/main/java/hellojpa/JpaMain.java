5148c7cf3f5e9724a63ed855e77f02c0f3153201
yeongsang2 <dudtkd1221@gmail.com>
2022-07-08 15:49:32 +0900

    객체지향 쿼리언어 기본문법1

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -1,9 +1,15 @@
 package hellojpa;
 
+import org.hibernate.Criteria;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import java.awt.*;
 import java.lang.management.MemoryManagerMXBean;
 import java.time.LocalDateTime;
 import java.util.List;
@@ -20,42 +26,12 @@ public class JpaMain {
 
         try {
 
-            Member member = new Member();
-            member.setUsername("username");
-            member.setHomeAddress(new Address("homeCity","street", "10000" ));
+            CriteriaBuilder cb = em.getCriteriaBuilder();
+            CriteriaQuery<Member> query = cb.createQuery(Member.class);
 
-            member.getFavoriteFoods().add("치킨");
-            member.getFavoriteFoods().add("피자");
-            member.getFavoriteFoods().add("족발");
-
-            member.getAddressHistory().add(new Address("old1","street", "10000" ));
-            member.getAddressHistory().add(new Address("old2","street", "10000" ));
-
-            em.persist(member);
-
-            em.flush();
-            em.clear();
-
-            System.out.println("============= START ==========");
-            Member findMember = em.find(Member.class, member.getId());
-
-            List<Address> addressHistory = findMember.getAddressHistory();
-            for(Address address :addressHistory){
-                System.out.println("address = " + address.getCity());
-            }
-
-            Set<String> favoriteFoods = findMember.getFavoriteFoods();
-            for(String favoriteFood : favoriteFoods){
-                System.out.println("favoriteFood = " + favoriteFood);
-            }
-
-            //findMember.getHomeAddress().setCity("newcity"); 수정하지 말고 새로 instance 삽입(완전 교체)
-            Address a = findMember.getHomeAddress();
-            findMember.setHomeAddress(new Address("newcity", a.getStreet(), a.getZipcode()));
-
-            findMember.getAddressHistory().remove(new Address("old2","street", "10000"));
-            findMember.getAddressHistory().add(new Address("newcity1","street", "10000"));
-
+            Root<Member> m = query.from(Member.class);
+            CriteriaQuery<Member> cq = query.select(m).where(cb.equal(m.get("username"), "kim"));
+            List<Member> resultList = em.createQuery(cq).getResultList();
 
             tx.commit(); //transaction.commit() 하 는 시점에 database 에 query 날림
         } catch (Exception e){
