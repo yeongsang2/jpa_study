72316a149d2631ae4b5a29e2b8a0d35fb7fe8302
yeongsang2 <dudtkd1221@gmail.com>
2022-07-10 23:59:16 +0900

    객체지향쿼리언어 - 페치 조인1

--- hellojpa/src/main/java/jpql/JpaMain.java
+++ hellojpa/src/main/java/jpql/JpaMain.java
@@ -16,19 +16,44 @@ public class JpaMain {
 
         try{
 
+            Team teamA = new Team();
+            teamA.setName("팀A");
+
+
+            Team teamB = new Team();
+            teamB.setName("팀B");
+
+            em.persist(teamA);
+            em.persist(teamB);
+
             Member member1 = new Member();
-            member1.setUsername("관리자1");
+            member1.setUsername("회원1");
+            member1.changeTeam(teamA);
             em.persist(member1);
 
             Member member2 = new Member();
-            member2.setUsername("관리자2");
+            member2.setUsername("회원2");
+            member2.changeTeam(teamB);
             em.persist(member2);
+
+            Member member3 = new Member();
+            member3.setUsername("회원3");
+            member3.changeTeam(teamB);
+            em.persist(member3);
+            System.out.println("==============================");
 
             em.flush();
             em.clear();
 
-            String query = "select m.team.name from Member m";
-            TypedQuery<Team> result = em.createQuery(query, Team.class);
+            String query = "select m from Member m";
+            System.out.println("==============================");
+            List<Member> result = em.createQuery(query, Member.class).getResultList();
+            System.out.println("==============================");
+
+            for(Member member : result){
+                System.out.println(member.getUsername() + " , " + member.getTeam().getName());
+            }
+
 
             tx.commit();
 
